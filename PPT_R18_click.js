290387b3f0d1fe391fb7cf2879446f523ce65fbe
永格天 <d1044111028@gm.lhu.edu.tw>
2017-12-11 22:24:59 +0800

    first_push
    就丟上來而已030.....

---
+++ PPT_R18_click.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         PPT_R18_click
+// @version      0.1
+// @description  auto_click_R18_button
+// @author       we684123
+// @match        https://www.ptt.cc/ask/over18?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    $("button.btn-big").click();
+})();
